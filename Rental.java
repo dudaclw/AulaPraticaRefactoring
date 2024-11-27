7dfac448c1dfc425d7a60db60c69657a01af3b65
Eduarda G. Kacprzak <128664709+dudaclw@users.noreply.github.com>
2024-11-27 01:33:35 +0000

    Commit 16

--- Rental.java
+++ Rental.java
@@ -1,5 +1,4 @@
 public class Rental {
-
     private Movie _movie;
     private int _daysRented;
 
@@ -15,25 +14,13 @@ public class Rental {
     public Movie getMovie() {
         return _movie;
     }
+
     public double getCharge() {
-        double thisAmount = 0;
-        switch (_movie.getPriceCode()) {
-            case Movie.REGULAR:
-                thisAmount += 2;
-                if (_daysRented > 2) {
-                    thisAmount += (_daysRented - 2) * 1.5;
-                }
-                break;
-            case Movie.NEW_RELEASE:
-                thisAmount += _daysRented * 3;
-                break;
-            case Movie.CHILDRENS:
-                thisAmount += 1.5;
-                if (_daysRented > 3) {
-                    thisAmount += (_daysRented - 3) * 1.5;
-                }
-                break;
-        }
-        return thisAmount;
+        return _movie.getCharge(_daysRented);
+    }
+
+    public int getFrequentRenterPoints() {
+        // Delegar cálculo para a classe Movie
+        return _movie.getFrequentRenterPoints(_daysRented);
     }
 }
